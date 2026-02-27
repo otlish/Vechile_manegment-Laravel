3e9fd9c4ffd342e294beaf98e7015b559d472d77
PC sujan <csit22081035_purushotam@achsnepal.edu.np>
2026-02-27 13:00:05 +0545

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/book/{vehicle}', [App\Http\Controllers\BookingController::class, 'create'])->name('bookings.create');
     Route::post('/book/{vehicle}', [App\Http\Controllers\BookingController::class, 'store'])->name('bookings.store');
     
+    // Review Routes
+    Route::post('/vehicles/{vehicle}/reviews', [App\Http\Controllers\ReviewController::class, 'store'])->name('reviews.store');
+
     // eSewa Payment Routes
     Route::get('/esewa/success', [App\Http\Controllers\EsewaController::class, 'success'])->name('esewa.success');
     Route::get('/esewa/failure', [App\Http\Controllers\EsewaController::class, 'failure'])->name('esewa.failure');
